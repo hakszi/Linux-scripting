6114dac5de2bcd610beaf44b2a5669239bc8c8de
hakszihu <62757269+hakszihu@users.noreply.github.com>
2024-03-22 19:34:05 +0000

    Convert Microsoft files to OpenDocument format
    Search for all files, also recursively inside folders and subfolders, and convert:
    - .docx/doc -> .odt
    - .xslx/xls -> .ods
    - .ppt/pptx/pps/ppsx -> odp

---
+++ convertms.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function convertms()
+{
+  tmp_files=$(mktemp).
+  if [[ ! -e $tmp_files ]]; then
+      touch $tmp_files
+  fi
+
+  find . -type f \( -name '*.docx' -o -name '*.doc' \) -printf %P\\n | sed -e 's/ /\\ /g' > "$tmp_files"
+
+  while read "p"; do
+      location="`readlink -f "$p" | rev | cut -d '/' -f2- | rev | sed -e 's/$/\//g'`"
+      libreoffice --headless --convert-to odt "$p" --outdir "$location" | sed 's/ /\\ /g' > /dev/null 2>&1
+      echo "$p -> `echo $p | cut -d . -f1 | xargs -I {} echo -n "{}.odt"`"
+  done <"$tmp_files"
+
+  
+
+  find . -type f \( -name '*.xlsx' -o -name '*.xls' \) -printf %P\\n | sed -e 's/ /\\ /g' > "$tmp_files"
+
+  while read "p"; do
+      location="`readlink -f "$p" | rev | cut -d '/' -f2- | rev | sed -e 's/$/\//g'`"
+      libreoffice --headless --convert-to ods "$p" --outdir "$location" | sed 's/ /\\ /g' > /dev/null 2>&1
+      echo "$p -> `echo $p | cut -d . -f1 | xargs -I {} echo -n "{}.ods"`"
+  done <"$tmp_files"
+
+
+
+  find . -type f \( -name '*.ppt' -o -name '*.pptx' -o -name '*.pps' -o -name '*.ppsx'  \)  -printf %P\\n | sed -e 's/ /\\ /g' > "$tmp_files"
+
+  while read "p"; do
+      location="`readlink -f "$p" | rev | cut -d '/' -f2- | rev | sed -e 's/$/\//g'`"
+      libreoffice --headless --convert-to odp "$p" --outdir "$location" | sed 's/ /\\ /g' > /dev/null 2>&1
+      echo "$p -> `echo $p | cut -d . -f1 | xargs -I {} echo -n "{}.pdf"`"
+  done <"$tmp_files"
+
+
+  find . -type f \( -name '*.docx' -o -name '*.doc' -o -name '*.xlsx' -o -name '*.xls' -o -name '*.ppt' -o -name '*.pptx' -o -name '*.pps' -o -name '*.ppsx' \) -exec mv -t "/tmp/" {} + 
+  
+  echo "Done! Moved every MS file to /tmp/ for easier recovery."
+  exiftool -r -overwrite_original -all= *
+};
