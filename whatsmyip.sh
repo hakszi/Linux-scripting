744b6064d8116359746dac447a46388b5ed8f916
hakszihu <62757269+hakszihu@users.noreply.github.com>
2024-03-22 19:54:33 +0000

    What's my ip script
    Convenient what's my ip script without having to use the browser.
    Provides information of:
    - interface
    - local ip
    - public ip
    - default gateway
    - primary DNS (ipv4 only)
    - secondary DNS (ipv4 only)
    - mac address
    - ip location
    
    The script utilizies "https://am.i.mullvad.net" online services for location information, and "https://ipinfo.io" for public ip. There were other ways, but comparing time required and convenience, this was much more worth it.

---
+++ whatsmyip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function whatsmyip()
+{
+intf=$(ip a | grep " UP " | cut -d ' ' -f 2 | cut -d ':' -f 1)
+localip=$(hostname -I | cut -d ' ' -f1 )
+publicip=$(curl --silent --show-error https://ipinfo.io/ip 2>&1)
+defaultgateway=$(ip r | grep default | cut -d ' ' -f3)
+dns=$(nmcli dev show | grep 'IP4.DNS' | awk '{print $2}' | tr '\n' ' ')
+mac=$(ip a | grep `hostname -I | cut -d ' ' -f1` -B 1 | grep "link/ether" | awk '{print $2}')
+city=$(curl --silent --show-error https://am.i.mullvad.net/city 2>&1)
+country=$(curl --silent --show-error https://am.i.mullvad.net/country 2>&1)
+
+echo "Interface in use: $intf"
+echo "Local IP: $localip"
+echo "Public IP: $publicip"
+echo "Default Gateway: $defaultgateway"
+echo -n "Primary & Secondary DNS (IPv4): "
+  echo $dns
+echo "Hardware MAC address: $mac"
+echo "Location: $country, $city"
+}
