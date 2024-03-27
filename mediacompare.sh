af205560873ad2c74c7ec0d53f6e4fda16964c5a
hakszihu <62757269+hakszihu@users.noreply.github.com>
2024-03-27 12:38:32 +0100

    Update to handle special characters
    Space, hyphen, etc...

--- mediacompare.sh
+++ mediacompare.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-files=`ls ./*`
+files=()
+while IFS= read -r -d $'\0' file; do
+    files+=("$file")
+done < <(find . -maxdepth 1 -type f -print0)
+
 tmp=$(mktemp)
 
-if [[ $1 == "" ]]; then
+if [[ -z "$1" ]]; then
     read -p "What extensions are your videos? " format
 else
     format=$1
 fi
 clear
 counter=0
-for i in $files
+for i in "${files[@]}"
 do
-    if [[ `echo $i | rev | cut -d '.' -f 1 | rev` == $format ]];then
-        echo -n `mediainfo --Inform="Video;%Duration%" "$i"` " " >> $tmp
-        echo `echo $i | cut -d '/' -f 2` >> $tmp
-        counter+=1
+    if [[ "${i##*.}" == "$format" ]];then
+        duration=$(mediainfo --Inform="Video;%Duration%" "$i" | cut -d '.' -f1)
+        echo -n "$duration    " >> $tmp
+        echo "$i" | cut -d '/' -f2- >> $tmp
+        ((counter++))
     fi
 done
 if [[ $counter == 0 ]]; then
-    echo "Sorry, nothing was found with the extension of \"$format\"."
+    echo "Sorry, nothing was found with the extension of \"$format\"." >&2
 else
-    sort -shr $tmp
+    sort -hr $tmp
 fi
 
 rm $tmp
+
